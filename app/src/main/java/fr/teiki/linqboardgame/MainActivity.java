1806ec796a6ee66d6f0c33ffa7e4f5a2e9fdc95c
Antoine <antoine.galtier@airinov.fr>
2016-03-08 11:20:22 +0100

    New words added Screeen Orientation lock

--- app/src/main/java/fr/teiki/linqboardgame/MainActivity.java
+++ app/src/main/java/fr/teiki/linqboardgame/MainActivity.java
@@ -2,13 +2,10 @@ package fr.teiki.linqboardgame;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -48,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         else
             finish();
 
-        if (nb_players > 0){
+        if (nb_players > 0) {
             game = new Game(nb_players);
             dispatchRole(0);
         }
--- app/src/main/java/fr/teiki/linqboardgame/PlayersNumberActivity.java
+++ app/src/main/java/fr/teiki/linqboardgame/PlayersNumberActivity.java
@@ -1,13 +1,12 @@
 package fr.teiki.linqboardgame;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class PlayersNumberActivity extends AppCompatActivity {
 
